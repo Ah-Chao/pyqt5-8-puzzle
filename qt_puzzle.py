7feea7f95b188f4d05b135af949213e747e66a97
Ah-Chao <278432384@qq.com>
2019-05-19 21:52:07 +0800

    增加定时器进行自动化游戏运行

--- qt_puzzle.py
+++ qt_puzzle.py
@@ -5,6 +5,8 @@
 
 import random
 import sys
+import traceback
+import time
 
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
@@ -48,7 +50,10 @@ class Container(QFrame):
         self.puzzle_state = None
         self.init_puzzle_state()
         self.solution = []
+        self.auto_timer = QBasicTimer()
         self.init_ui()
+
+
 
     def init_puzzle_state(self):
         self.puzzle_state = Puzzle()
@@ -73,6 +78,25 @@ class Container(QFrame):
         direction = self.KEY2MAP[key]
         self.puzzle_state.state_change_for_game(direction)
         self.init_ui()
+
+        if self.puzzle_state.state_success():
+            reply = QMessageBox.information(
+                self,
+                "成功",
+                "成功达到目的状态，是否继续游戏?",
+                QMessageBox.Yes | QMessageBox.No
+            )
+            if reply == QMessageBox.Yes:
+                self.init_puzzle_state()
+                self.init_ui()
+
+    def move_pic_by_direction(self, direction):
+
+        self.puzzle_state.state_change_for_game(direction)
+
+        print(self.puzzle_state)
+        self.init_ui()
+
         if self.puzzle_state.state_success():
             reply = QMessageBox.information(
                 self,
@@ -88,6 +112,7 @@ class Container(QFrame):
         key = event.key()
         if key in [Qt.Key_Left, Qt.Key_Right, Qt.Key_Down, Qt.Key_Up]:
             self.move_pic(key)
+
         elif key == Qt.Key_Q:
             try:
                 self.solution = get_move_sequence(self.puzzle_state)
@@ -102,11 +127,37 @@ class Container(QFrame):
                         self.init_puzzle_state()
                         self.init_ui()
                 else:
-                    print(self.solution)
+                    reply = QMessageBox.information(
+                        self,
+                        "提示",
+                        "已完成动作搜索，是否进行自动化运行?",
+                        QMessageBox.Yes | QMessageBox.No
+                    )
+                    print("Solution:",self.solution)
+                    if reply == QMessageBox.Yes:
+                        # 这里尝试了直接在函数中进行动作的转换，但是利用循环的方法导致没有从这个函数中走出来
+                        # 影响了底层的GUI绘图机制
+                        # 所以最好的办法，估计还是利用定时器的方法
+                        self.auto_timer.start(150,self)
 
 
-            except Exception as e:
-                print(e)
+            except Exception:
+                traceback.print_exc()
+
+    def timerEvent(self, event):
+        if event.timerId() == self.auto_timer.timerId():
+            try:
+                if len(self.solution) > 0:
+                    direction = self.solution.pop(0)
+                    self.move_pic_by_direction(direction)
+                else:
+                    self.auto_timer.stop()
+            except TypeError:
+                # 如果现在的状态已经是成功的，搜素返回值是0，引发类型错误
+                self.auto_timer.stop()
+            except Exception:
+                traceback.print_exc()
+
 
 
 def main():
